5fce7d770d708b8b65f44b05296c93c2bafdad74
Francisco Joaquín Jiménez Cordero <i22jicof@uco.es>
2026-02-19 11:42:06 +0100

    Commit Inicial
    Se ha creado el proyecto de SpringBoot para el enunciado 1
    
    Co-Authored-By: laikorai <150838847+laikorai@users.noreply.github.com>
    Co-Authored-By: JAVIER BERNAL CRIADO <p22becrj@uco.es>

---
+++ bloque1/enunciado1/factorymethod/factory_method/src/main/java/equipo10_mdas_bloque1_sesion1/factory_method/FactoryMethodApplication.java
@@ -0,0 +1,13 @@
+package equipo10_mdas_bloque1_sesion1.factory_method;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryMethodApplication.class, args);
+	}
+
+}
---
+++ bloque1/enunciado1/factorymethod/factory_method/src/test/java/equipo10_mdas_bloque1_sesion1/factory_method/FactoryMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package equipo10_mdas_bloque1_sesion1.factory_method;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
