e10a13aa3131d7f91269817fe0232b99a4eacc17
JAVIER BERNAL CRIADO <p22becrj@uco.es>
2026-04-03 20:10:37 +0200

    MAIN FACTORY
    Jane! - The long faces
    ARREGLADO QUE NO IBA EL CAMBIO DEL NOMBRE DE FICHERO A MAIN

--- bloque1/abstracfactory/src/factory_method/Main.java
+++ bloque1/abstracfactory/src/factory_method/Main.java
@@ -5,27 +5,21 @@ import factory_method.factories.FactoriaLlevar;
 import factory_method.factories.FactoriaRestaurante;
 import factory_method.menus.Menu;
 
-public class FactoryMethodApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		demo();
-	}
-
-	private static void demo() {
 		Factoria factoriaRestaurante = new FactoriaRestaurante();
 		Factoria factoriaLlevar      = new FactoriaLlevar();
 
         Menu menuRestaurante = factoriaRestaurante.crearMenuSemanal(TipoAcompanamiento.ENSALADA);
         Menu menuLlevar      = factoriaLlevar.crearMenuTemporada(TipoAcompanamiento.PATATAS);
-
-
-
 		System.out.println("MENU REST");
 		menuRestaurante.getPlatos();
 		System.out.println("MENU LLEVAR");
 		menuLlevar.getPlatos();
 
 	}
+
 
 
 }
